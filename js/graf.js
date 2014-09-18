febb302db28c348249c514e98f148716b9e05486
Марковец-Соболевский Алексей <deksss@i.ua>
2014-09-18 17:15:42 +0300

    начял пилить список тем

--- js/graf.js
+++ js/graf.js
@@ -6,6 +6,7 @@ var margin = {top: 20, right: 120, bottom: 20, left: 120},
 	width = 960 - margin.right - margin.left,
 	height = 500 - margin.top - margin.bottom;
 	
+var i = 0;
 
 var tree = d3.layout.tree()
 	.size([height, width]);
--- js/main.js
+++ js/main.js
@@ -9,8 +9,32 @@ $( document ).ready(function() {
   $("#goHome").bind("click", function() {
     $("#home").removeClass("hidden");
     $("#main").addClass("hidden");
-  });
-  
+  });  
+})(jQuery, window);
+
+
+(function($, windod) {
+function loadTheme () { 
+alert('yo!'); 
+};
+var itemShablon = ' <li><a id="{{id}}" href="#">{{name}}</a></li>';
+var id = 0;
+$.getJSON("js/listOftheme.json", function(json) {
+addListOfTheme(json);
+});
+function addListOfTheme ( listOfTheme ) {
+  listOfTheme.list.forEach(addItem);
+};
+function addItem (item) {
+  var itemHTML = itemShablon;
+  var itemId = "listThemeItem"+(id++);
+ itemHTML =  itemHTML.replace("{{name}}", item.name);
+  itemHTML =  itemHTML.replace("{{id}}", itemId);
+$("#listOfSubject").append(itemHTML);
+$("#"+itemId).bind("click", function(e) {
+   alert(item.file);
+  });  
+};
 })(jQuery, window);
 
 
