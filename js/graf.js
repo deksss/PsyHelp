9e466874649ff069fa10d04656c20c0a5d05bcb3
deksss <deksss@i.ua>
2014-09-21 20:53:00 +0300

    mmm

--- js/graf.js
+++ js/graf.js
@@ -1,101 +1,121 @@
-function drawGraf( data ) {
-  
-var margin = {top: 20, right: 120, bottom: 20, left: 120},
-	width = 960 - margin.right - margin.left,
-	height = 500 - margin.top - margin.bottom;
-	
-var i = 0;
+function drawGraf(data) {
 
-var tree = d3.layout.tree()
-	.size([height, width]);
+    var margin = {
+            top: 20,
+            right: 120,
+            bottom: 20,
+            left: 120
+        },
+        width = 960 - margin.right - margin.left,
+        height = 500 - margin.top - margin.bottom;
 
-var diagonal = d3.svg.diagonal()
-	.projection(function(d) { return [d.x, d.y]; });
-d3.select("svg")
-       .remove();
-var svg = d3.select("[id=diagramView]").append("svg")
-	.attr("width", width + margin.right + margin.left)
-	.attr("height", height + margin.top + margin.bottom)
-  .append("g")
-	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var i = 0;
 
-// load the external data
-d3.json(data, function(error, treeData) {
-  root = treeData[0];
-  update(root);
-});
+    var tree = d3.layout.tree()
+        .size([height, width]);
 
-function update(source) {
+    var diagonal = d3.svg.diagonal()
+        .projection(function(d) {
+            return [d.x, d.y];
+        });
+    d3.select("svg")
+        .remove();
+    var svg = d3.select("[id=diagramView]").append("svg")
+        .attr("width", width + margin.right + margin.left)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  // Compute the new tree layout.
-  var nodes = tree.nodes(root).reverse(),
-	  links = tree.links(nodes);
-	
+    // load the external data
+    d3.json(data, function(error, treeData) {
+        root = treeData[0];
+        update(root);
+    });
 
-  // Normalize for fixed-depth. тут вычисляеться растояние между нодами
-  nodes.forEach(function(d) { d.y = d.depth * 50; });
+    function update(source) {
 
-  // Declare the nodes…
-  var node = svg.selectAll("g.node")
-	  .data(nodes, function(d) { return d.id || (d.id = ++i); });
+        // Compute the new tree layout.
+        var nodes = tree.nodes(root).reverse(),
+            links = tree.links(nodes);
 
-  // Enter the nodes.
-  var nodeEnter = node.enter().append("g")
-	  .attr("class", "node")
-	  .attr("transform", function(d) { 
-		  return "translate(" + d.x + "," + d.y + ")"; });	  
- 
- var circles = nodeEnter.filter(function (d) {
-        return d.figure == "rhomb"
-    })
+
+        // Normalize for fixed-depth. тут вычисляеться растояние между нодами
+        nodes.forEach(function(d) {
+            d.y = d.depth * 50;
+        });
+
+        // Declare the nodes…
+        var node = svg.selectAll("g.node")
+            .data(nodes, function(d) {
+                return d.id || (d.id = ++i);
+            });
+
+        // Enter the nodes.
+        var nodeEnter = node.enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function(d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
+
+        var circles = nodeEnter.filter(function(d) {
+                return d.figure == "rhomb"
+            })
             .append("rect")
-            .attr("x", function(d) { d.x = d.x + 10; })
+            .attr("x", function(d) {
+                d.x = d.x + 10;
+            })
             .attr("width", 20)
             .attr("height", 20)
             .attr("rx", 1)
             .attr("ry", 1)
-            .attr("transform", "rotate(45)")
-            ;
+            .attr("transform", "rotate(45)");
 
-    var rect = nodeEnter.filter(function (d) {
-        return d.figure == "rect"
-    })
+        var rect = nodeEnter.filter(function(d) {
+                return d.figure == "rect"
+            })
             .append("rect")
-            .attr("x", function(d) { d.x = d.x + 10; })
+            .attr("x", function(d) {
+                d.x = d.x + 10;
+            })
             .attr("width", 20)
             .attr("height", 20)
             .attr("rx", 1)
-            .attr("ry", 1)
-            ;
+            .attr("ry", 1);
 
-              var rect = nodeEnter.filter(function (d) {
-        return d.figure == "line"
-    })
+        var rect = nodeEnter.filter(function(d) {
+                return d.figure == "line"
+            })
             .append("rect")
-            .attr("x", function(d) { d.x = d.x + 10; })
+            .attr("x", function(d) {
+                d.x = d.x + 10;
+            })
             .attr("width", 1)
             .attr("height", 1)
             .attr("rx", 1)
-            .attr("ry", 1)
-            ;
+            .attr("ry", 1);
 
-  nodeEnter.append("text")
-	  .attr("x", function(d) { 
-		  return d.children || d._children ? 30 : -13; })
-	  .attr("dy", ".35em")
-	  .attr("text-anchor", function(d) { 
-		  return d.children || d._children ? "end" : "start"; })
-	  .text(function(d) { return d.name; })
-	  .style("fill-opacity", 1);
+        nodeEnter.append("text")
+            .attr("x", function(d) {
+                return d.children || d._children ? 30 : -13;
+            })
+            .attr("dy", ".35em")
+            .attr("text-anchor", function(d) {
+                return d.children || d._children ? "end" : "start";
+            })
+            .text(function(d) {
+                return d.name;
+            })
+            .style("fill-opacity", 1);
 
-  // Declare the links…
-  var link = svg.selectAll("path.link")
-	  .data(links, function(d) { return d.target.id; });
+        // Declare the links…
+        var link = svg.selectAll("path.link")
+            .data(links, function(d) {
+                return d.target.id;
+            });
 
-  // Enter the links.
-  link.enter().insert("path", "g")
-	  .attr("class", "link")
-	  .attr("d", diagonal);
+        // Enter the links.
+        link.enter().insert("path", "g")
+            .attr("class", "link")
+            .attr("d", diagonal);
+    }
 }
-}
-
--- js/main.js
+++ js/main.js
@@ -1,108 +1,116 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
-(function($, windod) {
-  $("#getStart, #goShemes").bind("click", function() {
-    $("#main").removeClass("hidden");
-    $("#home").addClass("hidden");
-    $("#about").addClass("hidden");
-    $("#help").addClass("hidden");
-  }); 
-  $("#goHome").bind("click", function() {
-    $("#goHome").addClass("active");
-    $("#home").removeClass("hidden");
-    $("#main").addClass("hidden");
-    $("#about").addClass("hidden");
-    $("#help").addClass("hidden");
- });
-    $("#goHelp").bind("click", function() {
-   
-    if (!$("#goHelp").hasClass('active'))
-    {
-        $("#goHelp").parent().find('li').removeClass('active');
-        $("#goHelp").addClass('active');
+    (function($, windod) {
+        $("#getStart, #goShemes").bind("click", function() {
+            $("#main").removeClass("hidden");
+            $("#home").addClass("hidden");
+            $("#about").addClass("hidden");
+            $("#help").addClass("hidden");
+        });
+        $("#goHome").bind("click", function() {
+            $("#goHome").addClass("active");
+            $("#home").removeClass("hidden");
+            $("#main").addClass("hidden");
+            $("#about").addClass("hidden");
+            $("#help").addClass("hidden");
+        });
+        $("#goHelp").bind("click", function() {
+
+            if (!$("#goHelp").hasClass('active')) {
+                $("#goHelp").parent().find('li').removeClass('active');
+                $("#goHelp").addClass('active');
+            }
+            $("#help").removeClass("hidden");
+            $("#main").addClass("hidden");
+            $("#about").addClass("hidden");
+            $("#home").addClass("hidden");
+        });
+        $("#goAbout").bind("click", function() {
+            $("#goAbout").addClass("active");
+            $("#about").removeClass("hidden");
+            $("#main").addClass("hidden");
+            $("#home").addClass("hidden");
+            $("#help").addClass("hidden");
+        });
+    })(jQuery, window);
+
+
+    var pageController = {};
+
+    pageController.elementList = [{
+        "id": "#getStart",
+        "contentId": "#main"
+    }, {
+        "id": "#goShemes",
+        "contentId": "#main"
+    }, {
+        "id": "#goHome",
+        "contentId": "#home"
+    }, {
+        "id": "#goHelp",
+        "contentId": "#help"
+    }, {
+        "id": "#goAbou",
+        "contentId": "#about"
+    }];
+
+    pageController.contentList = [
+        "#main", "#home", "#help", "#about"
+    ]
+
+    pageController.controll = function() {
+        self = this;
+        self.elementList.forEach(self.addHndl);
     }
-    $("#help").removeClass("hidden");
-    $("#main").addClass("hidden");
-    $("#about").addClass("hidden");
-    $("#home").addClass("hidden");
- });
-    $("#goAbout").bind("click", function() {
-    $("#goAbout").addClass("active");
-    $("#about").removeClass("hidden");
-    $("#main").addClass("hidden");
-    $("#home").addClass("hidden");
-    $("#help").addClass("hidden");
-  });  
-})(jQuery, window);
+
+    pageController.addHndl = function(element) {
+        if (!$(element).hasClass('active')) {
+            $(element).parent().find('li').removeClass('active');
+            $(element).addClass('active');
+        }
+    }
 
 
-var pageController = {};
-
-pageController.elementList = 
-[
-{"id":"#getStart","contentId" : "#main"},
-{"id":"#goShemes","contentId" : "#main"},
-{"id":"#goHome","contentId" : "#home"},
-{"id":"#goHelp","contentId" : "#help"},
-{"id":"#goAbou","contentId" : "#about"}
-];
-
-pageController.contentList = [
-"#main","#home","#help","#about"
-]
-
-pageController.controll = function () {
-  self = this;
-  self.elementList.forEach(self.addHndl);
-}
-
-pageController.addHndl = function (element) {
-  if (!$(element).hasClass('active'))
-    {
-        $(element).parent().find('li').removeClass('active');
-        $(element).addClass('active');
-    } 
-}
+    var listOftheme = {
+        "itemShablon": ' <li><a id="{{id}}" href="#">{{name}}</a></li>',
+        "id": 0,
+        "curSelected": "null"
+    };
 
 
-var listOftheme = {
-"itemShablon" : ' <li><a id="{{id}}" href="#">{{name}}</a></li>',
-"id" : 0,
-"curSelected" : "null"
-};
+    listOftheme.drawActive = function(element) {
+        self = this;
+        element = element.parent();
+        if (self.curSelected !== "null") {
+            self.curSelected.removeClass("active");
+        }
+        self.curSelected = element;
+        element.addClass("active");
+    }
 
 
-listOftheme.drawActive = function (element) {
-self = this;
-element = element.parent();
-if (self.curSelected!=="null") {
-self.curSelected.removeClass("active");
-} 
-self.curSelected = element;
-element.addClass("active");
-}
+    listOftheme.load = function(drawGrafMethod, answerMethod, source) {
+        self = this;
+        $.getJSON(source, function(json) {
+            json.list.forEach(addItem);
+        });
 
+        function addItem(item) {
+          var target = $('#shemeApp');
+            var itemHTML = self.itemShablon;
+            var itemId = "listThemeItem" + (self.id++);
+            itemHTML = itemHTML.replace("{{name}}", item.name);
+            itemHTML = itemHTML.replace("{{id}}", itemId);
+            $("#listOfSubject").append(itemHTML);
+            $("#" + itemId).bind("click", function(e) {
+                $("#shemeApp").removeClass("hidden");
+                answerMethod(item.file);
+                drawGrafMethod(item.file);
+                self.drawActive($("#" + itemId));              
+            });
+        };
+    }
 
-listOftheme.load = function(drawGrafMethod, answerMethod, source) {
-self = this;
-$.getJSON(source, function(json) {
-json.list.forEach(addItem);
-});
-function addItem ( item ) {
-  var itemHTML = self.itemShablon;
-  var itemId = "listThemeItem"+(self.id++);
- itemHTML =  itemHTML.replace("{{name}}", item.name);
- itemHTML =  itemHTML.replace("{{id}}", itemId);
-$("#listOfSubject").append(itemHTML);
-$("#"+itemId).bind("click", function(e) {
-  answerMethod(item.file);
-  drawGrafMethod(item.file);
-  self.drawActive($("#"+itemId));
-  $("#shemeApp").removeClass("hidden");
-  });  
-};
-}
-
-listOftheme.load(drawGraf, answerApp,  "js/listOftheme.json");
+    listOftheme.load(drawGraf, answerApp, "js/listOftheme.json");
 
 });
